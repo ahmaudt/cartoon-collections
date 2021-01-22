807136227f0dd052e9969dff476db1c6176e5c02
Ahmaud R. Templeton <ahmaud@gmail.com>
2021-01-22 10:22:18 -0500

    commit tree ae4e67a470ec38b7001e6f0120580d639403c34a

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(names)# code an argument here
   # Your code here
+  names.map.with_index(1) { |name, num| puts "#{num}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(elements)# code an argument here
   # Your code here
+  elements.map! { |element| element.capitalize + "!" }
+  elements
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(words)# code an argument here
   # Your code here
+  # checks if word longer than for chars, returns true
+  words.any?{ |chars| chars.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(food)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese = ""
+    if food.include?("cheddar")
+      cheese = "cheddar"
+    elsif
+      food.include?("gouda")
+      cheese = "gouda"
+    elsif
+      food.include?("camembert")
+      cheese = "camembert"
+    else
+      cheese = nil
+    end  
+    cheese
 end
+
